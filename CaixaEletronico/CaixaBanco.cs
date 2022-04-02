8c647ab3d42981c16f55f56f59bba1bbd0d32fa6
Lucas Condé <lucasconde36@gmail.com>
2022-04-02 15:00:42 -0300

    Adicionar arquivos de projeto.

---
+++ CaixaEletronico/CaixaBanco.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaBanco
+{
+    class Caixa
+    {
+        public string Nome { get; private set; }
+        public double _Saldo { get; private set; }
+
+        private double _sacar;
+
+        private double _depositar;
+
+        public double Saldo
+        {
+            get
+            {
+                return _Saldo;
+            }
+        }
+
+        public void Saque(double vlrSaque)
+        {
+            if (_Saldo < vlrSaque || _Saldo == 0)
+            {
+                Console.WriteLine("Valor de saldo indisponível!");
+            }
+            else if (vlrSaque <= 0)
+            {
+                Console.WriteLine("Sem valor de saque, a operação será encerrada!");
+            }
+            else
+            {
+                _Saldo -= vlrSaque;
+                Console.Write("Valor sacado: R$" + vlrSaque);
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.Write("Saldo em conta: R$" + _Saldo);
+                Console.WriteLine();
+                Console.Write("----------------------------------");
+            }
+        }
+
+        public void Deposito(double vlrDeposito)
+        {
+            if (vlrDeposito <= 0)
+            {
+                Console.WriteLine("Sem valor de depósito, a operação será encerrada!");
+            }
+            else
+            {
+                _Saldo += vlrDeposito;
+                Console.Write("Valor Depositado: R$ " + vlrDeposito);
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.Write("Saldo em conta: R$ " + _Saldo);
+                Console.WriteLine();
+                Console.Write("----------------------------------");
+            }
+        }
+    }
+}
---
+++ CaixaEletronico/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaBanco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Caixa caixa = new Caixa();
+
+            Console.Title = "### Caixa Eletrônico ###";
+
+        Inicio:
+            Console.WriteLine("Escolha a opção na tela");
+
+            Console.WriteLine();
+
+            Console.WriteLine("1 - Depósito");
+            Console.WriteLine("2 - Saque");
+            Console.WriteLine("3 - Saldo em conta");
+
+            Console.WriteLine();
+
+            int Opcao = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            bool Sair = true;
+
+            while (Sair)
+            {
+
+                switch (Opcao)
+                {
+                    case 1:
+                        Console.Write("Digite o valor do depósito: ");
+                        double vlrDeposito = double.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        caixa.Deposito(vlrDeposito);
+                        Console.WriteLine();
+                        Console.WriteLine("Deseja finalizar a operação? (S/N)");
+                        string strSair = Console.ReadLine();
+                        Console.Clear();
+                        if (string.Equals(strSair, "S", StringComparison.OrdinalIgnoreCase))
+                        {
+                            Sair = false;
+                        }
+                        else
+                        {
+                            goto Inicio;
+                        }
+                        break;
+                    case 2:
+                        Console.Write("Digite o valor do saque: ");
+                        double vlrSaque = double.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        caixa.Saque(vlrSaque);
+                        Console.WriteLine();
+                        Console.WriteLine("Deseja finalizar a operação? (S/N)");
+                        strSair = Console.ReadLine();
+                        Console.Clear();
+                        if (string.Equals(strSair, "S", StringComparison.OrdinalIgnoreCase))
+                        {
+                            Sair = false;
+                        }
+                        else
+                        {
+                            goto Inicio;
+                        }
+                        break;
+                    case 3:
+                        Console.Write("Saldo em conta: " + caixa.Saldo);
+                        Console.WriteLine();
+                        Console.WriteLine("----------------------------------");
+                        Console.WriteLine("Deseja finalizar a operação? (S/N)");
+                        strSair = Console.ReadLine();
+                        Console.Clear();
+                        if (string.Equals(strSair, "S", StringComparison.OrdinalIgnoreCase))
+                        {
+                            Sair = false;
+                        }
+                        else
+                        {
+                            goto Inicio;
+                        }
+                        break;
+
+                    default: goto Inicio;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
